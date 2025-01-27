25dc1005aaa609472b53c15302afe31b3a0226f0
Rushi4511 <rushikarle45@gmail.com>
2025-01-27 23:22:02 +0530

    Print 2D matrix code

--- JAVA_DSA/SubArray/2DArray/PrintMatrix.java
+++ JAVA_DSA/SubArray/2DArray/PrintMatrix.java
@@ -1,11 +1,11 @@
 
 class PrintMatrix {
     public static void main(String[] args) {
-        int arr[][] = new int[][] { { 1, 2, 3, 4 }, { 4, 5, 6, 7 }, { 8, 9, 10, 11 } };
+        int arr[][] = new int[][] { { 1, 2, 3, 4 }, { 5, 6, 7, 8 }, { 9, 10, 11, 12 } };
         for (int i = 0; i < arr.length; i++) {
 
-            for (int j = i; j < arr[i].length; j++) {
-                System.out.println(arr[i][j]);
+            for (int j = 0; j < arr[i].length; j++) {
+                System.out.print(arr[i][j] + ",");
             }
             System.out.println();
         }
